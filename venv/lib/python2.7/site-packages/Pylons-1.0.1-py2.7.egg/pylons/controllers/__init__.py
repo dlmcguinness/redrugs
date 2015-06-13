d49857b1f291c0b3bdd0b25417ba42068ca73c62
micheldumontier <michel.dumontier@gmail.com>
2015-06-13 14:50:14 -0700

    removed venv from master

--- venv/lib/python2.7/site-packages/Pylons-1.0.1-py2.7.egg/pylons/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Standard Controllers intended for subclassing by web developers"""
-from pylons.controllers.core import WSGIController
-from pylons.controllers.xmlrpc import XMLRPCController
-from pylons.controllers.jsonrpc import JSONRPCController, JSONRPCError
--- venv/lib/python2.7/site-packages/devtools/commands/model_ming/session.py
+++
@@ -1,5 +0,0 @@
-from ming import Session
-from ming.odm import ThreadLocalORMSession
-
-mainsession = Session()
-DBSession = ThreadLocalORMSession(mainsession)
